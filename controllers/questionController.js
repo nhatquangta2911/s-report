81e1d99682c271815b4a74399e2a234daac38d58
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-11-07 16:48:06 +0700

    Fix Return Not Working

--- controllers/questionController.js
+++ controllers/questionController.js
@@ -16,7 +16,8 @@ const show_my_questions = async (req, res) => {
         userId: req.params.id
       }
     });
-    if (questions.length === 0) ErrorHelper.BadRequest(res, "User Not Found.");
+    if (questions.length === 0)
+      return ErrorHelper.BadRequest(res, "User Not Found.");
     res.json(questions);
   } catch (error) {
     logger.error(error.message, error);
@@ -36,7 +37,7 @@ const add_question = async (req, res) => {
       extraInfo: req.body.extraInfo
     });
     if (!req.body.ingredients || req.body.ingredients.length === 0)
-      ErrorHelper.BadRequest(res, "Question Has No Ingredients");
+      return ErrorHelper.BadRequest(res, "Question Has No Ingredients");
     await question.addIngredients(req.body.ingredients);
     res.json(question);
   } catch (error) {
