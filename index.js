7f7d00ed9d299d57949b5a428a2fe7bfb5d1c0ad
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-11-25 15:09:05 +0700

    Change default port to 5000

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const config = require('./config');
-const { logger } = require('./middlewares/logging');
+const express = require("express");
+const config = require("./config");
+const { logger } = require("./middlewares/logging");
 
 const app = express();
 
-require('./startup/logging')();
-require('./startup/routes')(app);
-require('./startup/prod')(app);
+require("./startup/logging")();
+require("./startup/routes")(app);
+require("./startup/prod")(app);
 
-const port = config.PORT || 3000;
+const port = config.PORT || 5000;
 
 const server = app.listen(port, () => {
   logger.info(`SERVER STARTED AT PORT ${port}...`);
