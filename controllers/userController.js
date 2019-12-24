c5797fc8dc394532bef58027724434f5feaabc0d
nhatquangta2911 <nhatquangta2911@gmail.com>
2019-12-25 02:23:58 +0700

    Active/Ban users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,10 @@
-const Sequelize = require("sequelize");
-const { logger } = require("../middlewares/logging");
+const Sequelize = require('sequelize');
+const { logger } = require('../middlewares/logging');
 const Op = Sequelize.Op;
-const bcrypt = require("bcryptjs");
-const ErrorHelper = require("../helpers/ErrorHelper");
-const config = require("../config");
-const { User, InfoUser, Role, Answer, Doctor } = require("../startup/db");
+const bcrypt = require('bcryptjs');
+const ErrorHelper = require('../helpers/ErrorHelper');
+const config = require('../config');
+const { User, InfoUser, Role, Answer, Doctor } = require('../startup/db');
 
 const show_all_user_info = async (req, res) => {
   try {
@@ -24,7 +24,7 @@ const find_User = async (req, res) => {
       where: { id: req.params.id },
       include: [InfoUser]
     });
-    if (!user) return ErrorHelper.NotFound(res, "User Not Found.");
+    if (!user) return ErrorHelper.NotFound(res, 'User Not Found.');
     res.send(user);
   } catch (error) {
     logger.error(error, error.message);
@@ -45,7 +45,7 @@ const show_all_users = async (req, res) => {
         },
         {
           model: Role,
-          attributes: ["name"],
+          attributes: ['name'],
           through: {
             attributes: []
           }
@@ -55,21 +55,21 @@ const show_all_users = async (req, res) => {
     res.json(users);
   } catch (error) {
     logger.error(error.message, error);
-    res.status(400).json("Something went wrong.");
+    res.status(400).json('Something went wrong.');
   }
 };
 
 const answer_question = async (req, res) => {
   try {
-    const userId = req.headers["id"];
+    const userId = req.headers['id'];
     const user = await User.findOne({ where: { id: userId } });
-    if (!user) return ErrorHelper.BadRequest(res, "User Not Found.");
+    if (!user) return ErrorHelper.BadRequest(res, 'User Not Found.');
     const answer = await Answer.create({
       answerContent: req.body.answerContent,
       positiveId: req.body.positiveId
     });
     if (!req.body.ingredients || req.body.ingredients.length === 0)
-      ErrorHelper.BadRequest(res, "Answer Has No Ingredients.");
+      ErrorHelper.BadRequest(res, 'Answer Has No Ingredients.');
     await answer.addIngredients(req.body.ingredients);
     await user.addAnswers([answer]);
     res.json(answer);
@@ -81,9 +81,9 @@ const answer_question = async (req, res) => {
 
 const add_doctor = async (req, res) => {
   try {
-    const userId = req.headers["id"];
+    const userId = req.headers['id'];
     let user = await User.findOne({ where: { id: userId } });
-    if (!user) return ErrorHelper.BadRequest(res, "User not found.");
+    if (!user) return ErrorHelper.BadRequest(res, 'User not found.');
     const result = await User.update(
       {
         ...user,
@@ -99,9 +99,9 @@ const add_doctor = async (req, res) => {
 
 const add_password_for_admin = async (req, res) => {
   try {
-    const userId = req.headers["id"];
+    const userId = req.headers['id'];
     let user = await User.findOne({ where: { id: userId } });
-    if (!user) return ErrorHelper.BadRequest(res, "User not found.");
+    if (!user) return ErrorHelper.BadRequest(res, 'User not found.');
     const salt = await bcrypt.genSalt(10);
     const password = await bcrypt.hash(req.body.password, salt);
     const result = await User.update(
@@ -117,11 +117,43 @@ const add_password_for_admin = async (req, res) => {
   }
 };
 
+const active_user = async (req, res) => {
+  try {
+    const userId = req.headers['id'];
+    let user = await User.findOne({ where: { id: userId } });
+    if (!user) return ErrorHelper.BadRequest(res, 'User not found.');
+    const result = await User.update(
+      { ...user, status: 1 },
+      { where: { id: userId } }
+    );
+    res.json(result);
+  } catch (error) {
+    ErrorHelper.InternalServerError(res, error);
+  }
+};
+
+const ban_user = async (req, res) => {
+  try {
+    const userId = req.headers['id'];
+    let user = await User.findOne({ where: { id: userId } });
+    if (!user) return ErrorHelper.BadRequest(res, 'User not found.');
+    const result = await User.update(
+      { ...user, status: 0 },
+      { where: { id: userId } }
+    );
+    res.json(result);
+  } catch (error) {
+    ErrorHelper.InternalServerError(res, error);
+  }
+};
+
 module.exports = {
   add_doctor,
   add_password_for_admin,
   show_all_user_info,
   show_all_users,
   find_User,
-  answer_question
+  answer_question,
+  active_user,
+  ban_user
 };
--- routes/users.js
+++ routes/users.js
@@ -1,23 +1,25 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const userController = require("../controllers/userController");
-const authController = require("../controllers/authController");
-const validateId = require("../middlewares/validateId");
-const auth = require("../middlewares/auth");
+const userController = require('../controllers/userController');
+const authController = require('../controllers/authController');
+const validateId = require('../middlewares/validateId');
+const auth = require('../middlewares/auth');
 
-router.get("/", userController.show_all_users);
-router.get("/user/:id", validateId, userController.find_User);
+router.get('/', userController.show_all_users);
+router.get('/user/:id', validateId, userController.find_User);
 
-router.post("/login", authController.login);
-router.post("/logout", authController.logout);
-router.post("/register", authController.register);
-router.post("/info/:id", authController.register_info);
+router.post('/login', authController.login);
+router.post('/logout', authController.logout);
+router.post('/register', authController.register);
+router.post('/info/:id', authController.register_info);
 
-router.get("/special", auth(["admin"]), (req, res) => res.json("Works!"));
-router.post("/upload", authController.demoUpload);
+router.get('/special', auth(['admin']), (req, res) => res.json('Works!'));
+router.post('/upload', authController.demoUpload);
 
-router.post("/submit", userController.answer_question);
-router.post("/doctor", userController.add_doctor);
-router.post("/password", userController.add_password_for_admin);
+router.post('/submit', userController.answer_question);
+router.post('/doctor', userController.add_doctor);
+router.post('/password', userController.add_password_for_admin);
+router.post('/active', userController.active_user);
+router.post('/ban', userController.ban_user);
 
 module.exports = router;
